02bb2975779cb6d449c95dbc558e9a184b32a4c4
sarthak-goognu <sarthak.m@goognu.com>
2026-09-21 11:35:34 +0530

    Update Next.js configuration options
    Set output to 'export', define basePath, and configure images.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/goognu-revamp",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
