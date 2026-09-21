c9c3f92f460a060c390c26fb55558b9fcc972510
sarthak-goognu <sarthak.m@goognu.com>
2026-09-21 10:19:15 +0530

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  // Your GitHub repository name
+  basePath: "/Goognu_revamp",
+
+  // Required when using GitHub Pages
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
